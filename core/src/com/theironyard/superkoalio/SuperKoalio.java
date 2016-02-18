29cd4ccfb61eb85ba79535d78a2a4d3a6a50018e
Alex <amhughes@g.cofc.edu>
2016-02-18 12:05:56 -0500

    did things

--- core/src/com/theironyard/superkoalio/SuperKoalio.java
+++ core/src/com/theironyard/superkoalio/SuperKoalio.java
@@ -2,22 +2,104 @@ package com.theironyard.superkoalio;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class SuperKoalio extends ApplicationAdapter {
     SpriteBatch batch;
+    TextureRegion stand, jump;
+    Animation walk;
+
+    static final int WIDTH = 18;
+    static final int HEIGHT = 26;
+    static final int DRAW_WIDTH = WIDTH * 3;
+    static final int DRAW_HEIGHT = HEIGHT * 3;
+    static final int GRAVITY = -50;
+    static final float MAX_VELOCITY = 100;
+    static final float MAX_JUMP_VELOCITY = 2000;
+    boolean canJump = true;
+    float x, y, xv, yv, time;
+    boolean faceRight = true;
+
 
     @Override
     public void create () {
+
         batch = new SpriteBatch();
+        Texture sheet = new Texture("koalio.png");
+        TextureRegion[][] tiles = TextureRegion.split(sheet, WIDTH, HEIGHT);
+        stand = tiles[0][0];
+        jump = tiles[0][1];
+        walk = new Animation(0.1f, tiles[0][2], tiles[0][3], tiles[0][4]);
     }
 
     @Override
     public void render () {
+        time += Gdx.graphics.getDeltaTime();
+
+        move();
+
+        TextureRegion img;
+        if (y > 0) {
+            img = jump;
+        }
+        else if (xv != 0) {
+            img = walk.getKeyFrame(time, true);
+        }
+        else {
+            img = stand;
+        }
+        Gdx.gl.glClearColor(0.5f, 0.5f, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        // draw here
+        if (faceRight){
+            batch.draw(img, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+        }
+        else {
+            batch.draw(img, x + DRAW_WIDTH, y, -1 * DRAW_WIDTH, DRAW_HEIGHT);
+        }
         batch.end();
+    }
+
+    float decelerate(float v, float deceleration) { //closer to 1 the slower the decelaration
+        v *= deceleration;
+        if (Math.abs(v) < 1) v = 0f;
+        return v;
+    }
+
+    void move() {
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) && canJump) {
+            yv = MAX_JUMP_VELOCITY;
+            canJump = false;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            yv = -1 * MAX_VELOCITY;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            xv = MAX_VELOCITY;
+            faceRight = true;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            xv = MAX_VELOCITY * -1;
+            faceRight = false;
+        }
+
+        yv += GRAVITY;
+
+        y += yv * Gdx.graphics.getDeltaTime();
+        x += xv * Gdx.graphics.getDeltaTime();
+
+        if (y < 0) {
+            y = 0;
+            canJump = true;
+        }
+
+
+        yv = decelerate(yv, 0.99f);
+        xv = decelerate(xv,.5f);
     }
 }
